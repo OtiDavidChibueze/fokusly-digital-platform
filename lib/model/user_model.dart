2f3fb89fc208c73d3c3c07bb3e9daaab3bcf8d20
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-06-18 05:07:54 +0100

    intial commit

---
+++ lib/model/user_model.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+part 'user_model.g.dart';
+
+@HiveType(typeId: 0)
+class UserModel extends HiveObject {
+  @HiveField(0)
+  String fullname;
+
+  @HiveField(1)
+  String email;
+
+  @HiveField(2)
+  String password;
+
+  UserModel({
+    required this.fullname,
+    required this.email,
+    required this.password,
+  });
+}
